39096933c658c67fc1ae275f79bd3cb37cfcbb7b
Nicholas <nkalpaxis@gmail.com>
2023-03-31 07:18:02 -0400

    merged cleaning scripts into one, refactor merge script

--- cleaning_scripts/cleaning.py
+++ cleaning_scripts/cleaning.py
@@ -1,11 +1,47 @@
 import pandas as pd
 
+# download player list from fanduel contest & rename it fdmlb.csv
+
+# read in fanduel player list csv
+players = pd.read_csv("fd.csv")
+
+# drop columns not being used
+players = players.drop(
+    [
+        "Id",
+        "First Name",
+        "Last Name",
+        "FPPG",
+        "Played",
+        "Game",
+        "Injury Indicator",
+        "Injury Details",
+        "Tier",
+        "Probable Pitcher",
+        "Batting Order",
+        "Roster Position",
+    ],
+    axis=1,
+)
+
+# remove pitchers from players list
+players = players[players.Position != "P"]
+
+# remove periods and apostrophes from players names
+players["Nickname"] = players["Nickname"].str.replace("'", "")
+players["Nickname"] = players["Nickname"].str.replace(".", "")
+
+# write over original csv
+players.to_csv("fd.csv", index=False)
+
+
 # read in csv files
 vsleft = pd.read_csv("vsLHP.csv")
 vsleft_bb = pd.read_csv("vsLHP bb.csv")
 vsright = pd.read_csv("vsRHP.csv")
 vsright_bb = pd.read_csv("vsRHP bb.csv")
 pitcher_stats = pd.read_csv("pitcher_stats.csv")
+barrels = pd.read_csv("barrels.csv")
 
 # drop columns not being used
 vsleft = vsleft.drop(
@@ -138,3 +174,24 @@ pitcher_stats["Name"] = pitcher_stats["Name"].str.replace("'", "")
 pitcher_stats["Name"] = pitcher_stats["Name"].str.replace(".", "")
 
 pitcher_stats.to_csv("pitcher_stats.csv", index=False)
+
+barrels = barrels.drop(
+    [
+        "Team",
+        "PA",
+        "Events",
+        "EV",
+        "maxEV",
+        "LA",
+        "AVG",
+        "xBA",
+        "SLG",
+        "xSLG",
+        "wOBA",
+        "xwOBA",
+        "playerid",
+    ],
+    axis=1,
+)
+
+barrels.to_csv("barrels.csv", index=False)
